a2b06176323d27652b77968552b8e52874cadb18
Nickzapryagaev <impulse.nn@gmail.com>
2022-11-14 03:03:28 +0600

    -

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -3,12 +3,13 @@ package ru.job4j.chess;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.PawnBlack;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
 public class LogicTest {
 
     @Test
@@ -19,5 +20,32 @@ public class LogicTest {
             logic.move(Cell.C1, Cell.H6);
         });
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    public void whenMoveThenOccupiedCellException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        BishopBlack bb = new BishopBlack(Cell.C8);
+        PawnBlack pb = new PawnBlack(Cell.D7);
+        logic.add(bb);
+        logic.add(pb);
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
+            logic.move(Cell.C8, Cell.G4);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Can't move through cell " + pb.position());
+    }
+
+    @Test
+    public void whenMoveThenImpossibleMoveException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        BishopBlack bb = new BishopBlack(Cell.C8);
+        logic.add(bb);
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
+            logic.move(Cell.C8, Cell.G5);
+        });
+        String result = String.format("Could not move by diagonal from %s to %s", Cell.C8, Cell.G5);
+        assertThat(exception.getMessage()).isEqualTo(result);
     }
 }
